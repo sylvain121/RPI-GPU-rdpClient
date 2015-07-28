7ecc90b7315531fe89dab4f6a107dc6a6b3235ef
sylvain MATHIEU <sylvain.mathieu@orange.com>
2015-07-28 15:02:29 +0200

    update

--- sdl_client/client.c
+++ sdl_client/client.c
@@ -28,6 +28,10 @@
 #include <SDL/SDL_thread.h>
 #include <SDL/SDL_net.h>
 
+
+#define INBUF_SIZE 1000000
+#define FF_INPUT_BUFFER_PADDING_SIZE 32
+
 #ifdef __MINGW32__
 #undef main /* Prevents SDL from overriding main() */
 #endif
@@ -43,96 +47,36 @@
 
 
 int main(int argc, char *argv[]) {
-  
-	IPaddress ip;
-	TCPsocket sd;
-	int quit, len;
-	char buffer[512];
 
-	if(SDLNet_Init() < 0 ) {
-		fprintf(stderr, "SDLNet_Init: %s\n", SDLNet_GetError());
-		exit(EXIT_FAILURE);
-	}
-
-	if(SDLNet_ResolveHost(&ip, argv[1], atoi(argv[2])) < 0) {
-		fprintf(stderr, "unable to resolve address %s , port %s \n", argv[1], argv[2]);
-		exit(1);
-	} 
-
-	if(!(sd = SDLNet_TCP_Open(&ip))) {
-		fprintf(stderr, "SDLNet_TCP_Open: %s\n", SDLNet_GetError());
-		exit(EXIT_FAILURE);	
-	}
-	
-	//getting h264 flow 
-	char sendKey[1] = {'a'};
-	SDLNet_TCP_Send(sd, (void * )sendKey, len);
-
-
-
-	// received data loop
-
-
-	while(1) {
-		SDLNet_TCP_Recv(sd, buffer, 512);
-		printf("%s\n", buffer);
-
-	}
-
-/**
   AVFormatContext *pFormatCtx = NULL;
   int             i, videoStream;
-  AVCodecContext  *pCodecCtxOrig = NULL;
   AVCodecContext  *pCodecCtx = NULL;
   AVCodec         *pCodec = NULL;
   AVFrame         *pFrame = NULL;
-  AVPacket        packet;
-  int             frameFinished;
   float           aspect_ratio;
   struct SwsContext *sws_ctx = NULL;
-
+  AVCodecParserContext *parser = NULL;
+  int pts, dts;
   SDL_Overlay     *bmp;
   SDL_Surface     *screen;
   SDL_Rect        rect;
   SDL_Event       event;
 
-  if(argc < 2) {
-    fprintf(stderr, "Usage: test <file>\n");
-    exit(1);
-  }
+
+  uint8_t inbuf[INBUF_SIZE + FF_INPUT_BUFFER_PADDING_SIZE];
+  memset(inbuf + INBUF_SIZE, 0, FF_INPUT_BUFFER_PADDING_SIZE);
+
   // Register all formats and codecs
   av_register_all();
+  avformat_network_init();
   
   if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER)) {
     fprintf(stderr, "Could not initialize SDL - %s\n", SDL_GetError());
     exit(1);
   }
 
-  // Open video file
-  if(avformat_open_input(&pFormatCtx, argv[1], NULL, NULL)!=0)
-    return -1; // Couldn't open file
-  
-  // Retrieve stream information
-  if(avformat_find_stream_info(pFormatCtx, NULL)<0)
-    return -1; // Couldn't find stream information
-  
-  // Dump information about file onto standard error
-  av_dump_format(pFormatCtx, 0, argv[1], 0);
-  
-  // Find the first video stream
-  videoStream=-1;
-  for(i=0; i<pFormatCtx->nb_streams; i++)
-    if(pFormatCtx->streams[i]->codec->codec_type==AVMEDIA_TYPE_VIDEO) {
-      videoStream=i;
-      break;
-    }
-  if(videoStream==-1)
-    return -1; // Didn't find a video stream
-  
-  // Get a pointer to the codec context for the video stream
-  pCodecCtxOrig=pFormatCtx->streams[videoStream]->codec;
-  // Find the decoder for the video stream
-  pCodec=avcodec_find_decoder(pCodecCtxOrig->codec_id);
+    // Find the decoder for the video stream
+  pCodec=avcodec_find_decoder(AV_CODEC_ID_H264);
   if(pCodec==NULL) {
     fprintf(stderr, "Unsupported codec!\n");
     return -1; // Codec not found
@@ -140,10 +84,13 @@ int main(int argc, char *argv[]) {
 
   // Copy context
   pCodecCtx = avcodec_alloc_context3(pCodec);
-  if(avcodec_copy_context(pCodecCtx, pCodecCtxOrig) != 0) {
-    fprintf(stderr, "Couldn't copy codec context");
-    return -1; // Error copying codec context
-  }
+  pCodecCtx->width = 1280;
+  pCodecCtx->height = 900;
+  pCodecCtx->pix_fmt = AV_PIX_FMT_YUV420P;
+
+
+  pCodecCtx->flags|= CODEC_FLAG_TRUNCATED;
+
 
   // Open codec
   if(avcodec_open2(pCodecCtx, pCodec, NULL)<0)
@@ -181,70 +128,123 @@ int main(int argc, char *argv[]) {
 			   NULL,
 			   NULL
 			   );
+/**
+*
+*
+* NETWORK
+*
+*/
 
-
-
-  // Read frames and save first five frames to disk
-  i=0;
-  while(av_read_frame(pFormatCtx, &packet)>=0) {
-    // Is this a packet from the video stream?
-    if(packet.stream_index==videoStream) {
-      // Decode video frame
-      avcodec_decode_video2(pCodecCtx, pFrame, &frameFinished, &packet);
-      
-      // Did we get a video frame?
-      if(frameFinished) {
-	SDL_LockYUVOverlay(bmp);
-
-	AVPicture pict;
-	pict.data[0] = bmp->pixels[0];
-	pict.data[1] = bmp->pixels[2];
-	pict.data[2] = bmp->pixels[1];
-
-	pict.linesize[0] = bmp->pitches[0];
-	pict.linesize[1] = bmp->pitches[2];
-	pict.linesize[2] = bmp->pitches[1];
-
-	// Convert the image into YUV format that SDL uses
-	sws_scale(sws_ctx, (uint8_t const * const *)pFrame->data,
-		  pFrame->linesize, 0, pCodecCtx->height,
-		  pict.data, pict.linesize);
-
-	SDL_UnlockYUVOverlay(bmp);
-	
-	rect.x = 0;
-	rect.y = 0;
-	rect.w = pCodecCtx->width;
-	rect.h = pCodecCtx->height;
-	SDL_DisplayYUVOverlay(bmp, &rect);
-      
-      }
-    }
-    
-    // Free the packet that was allocated by av_read_frame
-    av_free_packet(&packet);
-    SDL_PollEvent(&event);
-    switch(event.type) {
-    case SDL_QUIT:
-      SDL_Quit();
-      exit(0);
-      break;
-    default:
-      break;
-    }
-
-  }
   
+	IPaddress ip;
+	TCPsocket sd;
+	int quit, len;
+
+	if(SDLNet_Init() < 0 ) {
+		fprintf(stderr, "SDLNet_Init: %s\n", SDLNet_GetError());
+		exit(EXIT_FAILURE);
+	}
+
+	if(SDLNet_ResolveHost(&ip, argv[1], atoi(argv[2])) < 0) {
+		fprintf(stderr, "unable to resolve address %s , port %s \n", argv[1], argv[2]);
+		exit(1);
+	} 
+
+	if(!(sd = SDLNet_TCP_Open(&ip))) {
+		fprintf(stderr, "SDLNet_TCP_Open: %s\n", SDLNet_GetError());
+		exit(EXIT_FAILURE);	
+	}
+	
+	//getting h264 flow 
+	printf("sending h264 request packet\n");
+	char sendKey[1] = {'a'};
+	len = strlen(sendKey);
+	SDLNet_TCP_Send(sd, (void * )sendKey, len);
+
+	// received data loop
+	AVPacket        packet;
+	packet.data = NULL;
+	packet.size = 0;
+
+	parser = av_parser_init(pCodecCtx->codec_id);
+    parser->flags |= PARSER_FLAG_ONCE;
+    int parserLenght = 0;
+    char net_in[INBUF_SIZE];
+    int inbuf_average = 0;
+
+
+	for(;;) {
+		av_init_packet(&packet);
+		int net_lenght =SDLNet_TCP_Recv(sd, net_in, INBUF_SIZE);
+		memcpy(inbuf+inbuf_average, net_in, net_lenght);
+		printf("inbuf_average %i  + network length %i \n", inbuf_average, net_lenght);
+		inbuf_average = net_lenght + inbuf_average;
+		printf("result inbuf_average %i \n", inbuf_average);
+
+	
+		parserLenght = av_parser_parse2(parser, pCodecCtx, &packet.data, &packet.size, inbuf, inbuf_average, pts, dts, AV_NOPTS_VALUE);
+		memcpy(inbuf, inbuf+parserLenght, inbuf_average - parserLenght);
+		inbuf_average -= parserLenght;
+
+		if(net_lenght == 0)
+			break;
+
+		while(packet.size > 0) {
+
+			int lenght;
+			// Decode video frame
+			  int frameFinished;
+		      lenght = avcodec_decode_video2(pCodecCtx, pFrame, &frameFinished, &packet);
+		      if( lenght < 0 ) {
+		      	fprintf(stderr, "Error while decoding frame\n");
+		      }
+		      // Did we get a video frame?
+		      if(frameFinished) {
+			SDL_LockYUVOverlay(bmp);
+
+			AVPicture pict;
+			pict.data[0] = bmp->pixels[0];
+			pict.data[1] = bmp->pixels[2];
+			pict.data[2] = bmp->pixels[1];
+
+			pict.linesize[0] = bmp->pitches[0];
+			pict.linesize[1] = bmp->pitches[2];
+			pict.linesize[2] = bmp->pitches[1];
+
+			// Convert the image into YUV format that SDL uses
+			sws_scale(sws_ctx, (uint8_t const * const *)pFrame->data,
+				  pFrame->linesize, 0, pCodecCtx->height,
+				  pict.data, pict.linesize);
+
+			SDL_UnlockYUVOverlay(bmp);
+			
+			rect.x = 0;
+			rect.y = 0;
+			rect.w = pCodecCtx->width;
+			rect.h = pCodecCtx->height;
+			SDL_DisplayYUVOverlay(bmp, &rect);
+   
+	      }
+	      	if(packet.data)
+			{
+				packet.size -=lenght;
+				packet.data +=lenght;
+			}
+		}
+		av_free_packet(&packet);
+		
+	}
+
+
   // Free the YUV frame
   av_frame_free(&pFrame);
   
   // Close the codec
   avcodec_close(pCodecCtx);
-  avcodec_close(pCodecCtxOrig);
   
   // Close the video file
   avformat_close_input(&pFormatCtx);
   
   return 0;
-  */
+  
 }
