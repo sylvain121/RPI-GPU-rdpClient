a2a783b9ab2392fad29130738bf4fa816cb01509
sylvain MATHIEU <sylvain.mathieu@orange.com>
2015-07-31 10:23:51 +0200

    refactoring cmakefile

---
+++ Server/linux/X11Capture.cpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Capture.h"
+
+class X11Capture : public Capture {
+public : 
+
+	void init(UINT monitorID, RECT screen) 
+
+	{
+
+	}
+
+	int getNextFrame(RGBQUAD** data)
+	{
+
+	}
+
+	void doneNextFrame()
+	{
+		
+	}
+
+}
